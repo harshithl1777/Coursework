e37bfc3baae33b06627fe9e70e609a68992289db
Harshith Latchupatula <harshithl1777@gmail.com>
2021-04-09 13:25:23 -0400

    finished redux store design section

--- React Course/blog/src/actions/index.js
+++ React Course/blog/src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+}
+
 export const fetchPosts = () => async dispatch => {
     const { data } = await jsonPlaceholder.get('/posts');
     dispatch({ type: 'FETCH_POSTS', payload: data });
--- React Course/blog/src/components/UserHeader/UserHeader.js
+++ React Course/blog/src/components/UserHeader/UserHeader.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../../actions';
 
 class UserHeader extends Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
-
     render() {
         return (
             <div className='header'>{(this.props.user) ? this.props.user.name : 'Loading...'}</div>
@@ -18,4 +13,4 @@ const mapStateToProps = ({ users }, ownProps) => {
     return { user: users.find(user => user.id === ownProps.userId) }; 
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
