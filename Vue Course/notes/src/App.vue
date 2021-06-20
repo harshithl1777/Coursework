6f54ef00cb3c576710256c834b6f41d12463eb5e
Harshith Latchupatula <harshithl1777@gmail.com>
2021-06-19 20:37:44 -0400

    added first vue project

---
+++ Vue Course/notes/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+</template>
+
+<script>
+  import Header from '@/components/Header/Header.vue'
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style>
+  @import './app.css';
+</style>
---
+++ Vue Course/notes/src/components/Header/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class='notesHeader'>
+        <h1 class='notesLogo'>Notes.io</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+    }
+</script>
+
+<style>
+    @import './header.css';
+</style>
